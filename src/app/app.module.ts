3f8e3072b0f63c448c039ea68025fef0f8b15d8b
Tetyana Shvets <tshvets@hotmail.com>
2018-09-09 17:39:41 -0700

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,15 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,7 @@ import { MenuComponent } from './menu/menu.component';
     MatButtonModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
